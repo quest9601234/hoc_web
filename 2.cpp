9cc4a619166a9354a215400fa1d5bb5bea6e5f1c
Quy Nguyen <nguyendinhquy155012345@gmail.com>
2018-11-12 22:02:51 +0700

    thu xem dung git nhu the nao

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"moi ban nhap n :"<<endl;
+	cin>>n;
+	if(n>100)
+		cout<<"so nhap vao lon hon 100";
+	//if(n=100)
+	//	cout<<"so nhap vao dug bang 100";
+	else
+		cout<<"so nhap vao nho hon 100";
+}
